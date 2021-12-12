ea7c8cf06064c5c7e139f0b20a54d8c8e6b050aa
Achmad Naila Muna Ramadhani <79986070+armdhni12@users.noreply.github.com>
2021-12-12 19:44:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 def cek_data():
     return render_template('https://data.jakarta.go.id/dataset/indeks-standar-pencemaran-udara-ispu-tahun-2021')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET','POST'])
 # def __init__(self,pm10,pm25,so2,co,o3,no2):
 #     self.pm10=pm10
 #     self.pm25=pm25
